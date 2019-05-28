2b64a92ad097caf5a9b946e7aa7a05cc6d8d7050
Dean Barker <deanbarker44@gmail.com>
2019-05-28 16:29:15 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+let { soundManager } = require('soundmanager2');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const audioUrl = 'https://lingomastery.s3.amazonaws.com/audio/A0rvqIm27.mp3';
+const min = 10;
+const sec = 2.2;
+const msToPlayFrom = (min*60*1000)+(sec*1000);
+
+class App extends Component {
+
+  playSound(){
+    let sound = soundManager.createSound({
+      id: 'mySound',
+      url: audioUrl,
+      autoLoad: true,
+      autoPlay: false,
+      onload: function() {
+        this.play({
+          position: msToPlayFrom
+        })
+      },
+      volume: 50
+    });
+  }
+
+  render(){
+    return (
+      <div >
+      <p>firefox is accurate and starts at the word "cometí", while chrome starts earlier at "preparando" </p>
+  
+     <button onClick={this.playSound}>Play</button>
+      </div>
+    );
+
+  }
 }
 
 export default App;
