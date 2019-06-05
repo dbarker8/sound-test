ee78989df5bec8b4af457eea47ec3bfee638adf7
Dean Barker <deanbarker44@gmail.com>
2019-06-05 15:42:46 -0400

    audio api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
-import React, {Component} from 'react';
-let { soundManager } = require('soundmanager2');
+import React, { Component } from "react";
+let { soundManager } = require("soundmanager2");
 
-const audioUrl = 'https://lingomastery.s3.amazonaws.com/audio/A0rvqIm27.mp3';
+const audioUrl = "https://lingomastery.s3.amazonaws.com/audio/A0rvqIm27.mp3";
 const min = 10;
 const sec = 2.2;
-const msToPlayFrom = (min*60*1000)+(sec*1000);
+const msToPlayFrom = min * 60 * 1000 + sec * 1000;
 
 class App extends Component {
-
-  playSound(){
+  playSound() {
     let sound = soundManager.createSound({
-      id: 'mySound',
+      id: "mySound",
       url: audioUrl,
       autoLoad: true,
       autoPlay: false,
       onload: function() {
         this.play({
           position: msToPlayFrom
-        })
+        });
       },
       volume: 50
     });
   }
 
-  render(){
+  playSoundAudioApi() {
+    this.sound = new Audio(audioUrl);
+    this.sound.currentTime = msToPlayFrom / 1000;
+    this.sound.play();
+  }
+
+  render() {
     return (
-      <div >
-      <p>firefox is accurate and starts at the word "cometí", while chrome starts earlier at "preparando" </p>
-  
-     <button onClick={this.playSound}>Play</button>
+      <div>
+        <p>
+          firefox is accurate and starts at the word "cometí", while chrome
+          starts earlier at "preparando"{" "}
+        </p>
+
+        <button onClick={this.playSound} style={{ marginRight: 20 }}>
+          Play
+        </button>
+        <button onClick={this.playSoundAudioApi.bind(this)}>
+          Play using html audio
+        </button>
       </div>
     );
-
   }
 }
 
